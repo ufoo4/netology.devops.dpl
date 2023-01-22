6d6a039e616f7675b1e0558c693b242b6255d303
Sergey Chipyshev <schipyshev@stsoft.ru>
2023-01-22 15:26:04 +0500

    add tf cloud

---
+++ src/terraform/provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+}
+
+provider "yandex" {
+  cloud_id  = "${var.yandex_cloud_id}"
+  folder_id = "${var.yandex_folder_id}"
+}
+
+terraform {
+  cloud {
+    organization = "netology-dpl"
+
+    workspaces {
+      name = "dpl-terraform-cloud"
+    }
+  }
+}
