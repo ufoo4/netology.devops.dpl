953f09e4a0c0e101d0455348e90ef0b388068049
Sergey Chipyshev <schipyshev@stsoft.ru>
2023-01-27 00:28:26 +0500

    edit net

--- terraform/network.tf
+++ terraform/network.tf
@@ -38,26 +38,26 @@ resource "yandex_vpc_subnet" "public-subnet-c" {
   v4_cidr_blocks = ["192.168.12.0/24"]
 }
 
-resource "yandex_vpc_subnet" "private-subnet-a" {
-  name           = "private-a"
-  zone           = "ru-central1-a"
-  network_id     = "${yandex_vpc_network.net.id}"
-  route_table_id = "${yandex_vpc_route_table.private-to-inet.id}"
-  v4_cidr_blocks = ["192.168.20.0/24"]
-}
+# resource "yandex_vpc_subnet" "private-subnet-a" {
+#   name           = "private-a"
+#   zone           = "ru-central1-a"
+#   network_id     = "${yandex_vpc_network.net.id}"
+#   route_table_id = "${yandex_vpc_route_table.private-to-inet.id}"
+#   v4_cidr_blocks = ["192.168.20.0/24"]
+# }
 
-resource "yandex_vpc_subnet" "private-subnet-b" {
-  name           = "private-b"
-  zone           = "ru-central1-b"
-  network_id     = "${yandex_vpc_network.net.id}"
-  route_table_id = "${yandex_vpc_route_table.private-to-inet.id}"
-  v4_cidr_blocks = ["192.168.21.0/24"]
-}
+# resource "yandex_vpc_subnet" "private-subnet-b" {
+#   name           = "private-b"
+#   zone           = "ru-central1-b"
+#   network_id     = "${yandex_vpc_network.net.id}"
+#   route_table_id = "${yandex_vpc_route_table.private-to-inet.id}"
+#   v4_cidr_blocks = ["192.168.21.0/24"]
+# }
 
-resource "yandex_vpc_subnet" "private-subnet-c" {
-  name           = "private-c"
-  zone           = "ru-central1-c"
-  network_id     = "${yandex_vpc_network.net.id}"
-  route_table_id = "${yandex_vpc_route_table.private-to-inet.id}"
-  v4_cidr_blocks = ["192.168.22.0/24"]
-}
+# resource "yandex_vpc_subnet" "private-subnet-c" {
+#   name           = "private-c"
+#   zone           = "ru-central1-c"
+#   network_id     = "${yandex_vpc_network.net.id}"
+#   route_table_id = "${yandex_vpc_route_table.private-to-inet.id}"
+#   v4_cidr_blocks = ["192.168.22.0/24"]
+# }
