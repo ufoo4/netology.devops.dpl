9b7365054fcf3ab24e7cbf4b391426af13b070f5
Sergey Chipyshev <schipyshev@stsoft.ru>
2023-01-27 16:01:04 +0500

    test net

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,31 +11,7 @@ variable "TF_VAR_YC_CREDENTIAL" {
   default = ""
 }
 
-# variable "TFC_WORKSPACE_NAME" {
-#   type    = string
-#   default = ""
-# }
-
-variable "network_cidr" {
-  type    = list(string)
-  default = {
-    stage = "10.0.0.0/16"
-    prod  = "10.1.0.0/16"
-  }
-}
-
-variable "private_subnet_cidrs" {
-  type = list(string)
-  default = {
-    stage = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
-    prod  = ["10.1.0.0/24", "10.1.1.0/24", "10.1.2.0/24"]
-  }
-}
-
-variable "public_subnet_cidrs" {
-  type = list(string)
-  default = {
-    stage = ["10.0.3.0/24", "10.0.4.0/24", "10.0.5.0/24"]
-    prod  = ["10.1.3.0/24", "10.1.4.0/24", "10.1.5.0/24"]
-  }
+variable "TFC_WORKSPACE_NAME" {
+  type    = string
+  default = ""
 }
