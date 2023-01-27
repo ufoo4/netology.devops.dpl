fef7d51c674187ee1a77176ffe86efce72679a67
Sergey Chipyshev <schipyshev@stsoft.ru>
2023-01-27 16:11:18 +0500

    test net4

--- terraform/network.tf
+++ terraform/network.tf
@@ -4,8 +4,8 @@ resource "yandex_vpc_network" "net" {
 
 resource "yandex_vpc_subnet" "public" {
   count          = length(local.networks)
-  v4_cidr_blocks = local.networks[count.index].subnet
-  zone           = local.networks[count.index].zone_name
+  v4_cidr_blocks = "local.networks${var.TFC_WORKSPACE_NAME}.[count.index].subnet"
+  zone           = "local.networks.${var.TFC_WORKSPACE_NAME}.[count.index].zone_name"
   network_id     = yandex_vpc_network.net.id
   name           = "subnet-${local.networks[count.index].zone_name}"
 }
