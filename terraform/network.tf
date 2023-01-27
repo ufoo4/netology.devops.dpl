77a227a0ddd22dc3541c300c3d611f4abebb4ee0
Sergey Chipyshev <schipyshev@stsoft.ru>
2023-01-27 17:11:33 +0500

    test net14

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,23 +1,23 @@
-resource "yandex_vpc_network" "net-stage" {
-  name = "net-stage"
+resource "yandex_vpc_network" "net" {
+  name = "net"
 }
 
 resource "yandex_vpc_subnet" "public-stage" {
   count          = length(local.networks)
   v4_cidr_blocks = local.networks[count.index].subnet
   zone           = local.networks[count.index].zone_name
-  network_id     = yandex_vpc_network.net-stage.id
-  name           = "subnet-${local.networks[count.index].zone_name}"
+  network_id     = yandex_vpc_network.net.id
+  name           = "${var.TFC_WORKSPACE_NAME}-${local.networks[count.index].zone_name}"
 }
 
-resource "yandex_vpc_network" "net-prod" {
-  name = "net-prod"
-}
+# resource "yandex_vpc_network" "net-prod" {
+#   name = "net-prod"
+# }
 
-resource "yandex_vpc_subnet" "public-prod" {
-  count          = length(local.networks)
-  v4_cidr_blocks = local.networks[count.index].subnet
-  zone           = local.networks[count.index].zone_name
-  network_id     = yandex_vpc_network.net-prod.id
-  name           = "subnet-${local.networks[count.index].zone_name}"
-}
+# resource "yandex_vpc_subnet" "public-prod" {
+#   count          = length(local.networks)
+#   v4_cidr_blocks = local.networks[count.index].subnet
+#   zone           = local.networks[count.index].zone_name
+#   network_id     = yandex_vpc_network.net-prod.id
+#   name           = "subnet-${local.networks[count.index].zone_name}"
+# }
