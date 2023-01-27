f47e855ac71bfd99b40098cc287d7f1eabac0e7b
Sergey Chipyshev <schipyshev@stsoft.ru>
2023-01-27 17:31:04 +0500

    test net15

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,8 +1,8 @@
 resource "yandex_vpc_network" "net" {
-  name = "net"
+  name = "net-${var.TFC_WORKSPACE_NAME}"
 }
 
-resource "yandex_vpc_subnet" "public-stage" {
+resource "yandex_vpc_subnet" "public" {
   count          = length(local.networks)
   v4_cidr_blocks = local.networks[count.index].subnet
   zone           = local.networks[count.index].zone_name
