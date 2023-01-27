5ace5c4b28041eb55f3625fa925b37bba4616aeb
Sergey Chipyshev <schipyshev@stsoft.ru>
2023-01-27 19:04:08 +0500

    add template docs

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,4 +1,10 @@
 locals {
+  default_zone = "local.networks.${var.TF_VAR_WORKSPACE_NAME}.0.zone_name"
+  # dns_zone     = "yc.complife.ru"
+  # cluster_name = "kube-cluster"
+  # control_url  = "${local.cluster_name}.${local.dns_zone}"
+  # ingress_url  = "${local.cluster_name}-ingress.${local.dns_zone}"
+
   networks = [
     {
       zone_name = "ru-central1-a"
@@ -14,48 +20,42 @@ locals {
     }
   ]
 
-  default_zone = "local.networks.${var.TF_VAR_WORKSPACE_NAME}.0.zone_name"
-  dns_zone     = "yc.complife.ru"
-  cluster_name = "kube-cluster"
-  control_url  = "${local.cluster_name}.${local.dns_zone}"
-  ingress_url  = "${local.cluster_name}-ingress.${local.dns_zone}"
-
-  k8s_cluster_resources = {
-    stage = {
-      masters = {
-        count       = 1
-        cpu         = 4
-        memory      = 4
-        disk        = 93
-        disk_type   = "network-ssd-nonreplicated"
-        preemptible = false
-      }
-      workers = {
-        count       = 3
-        cpu         = 2
-        memory      = 4
-        disk        = 20
-        disk_type   = "network-ssd"
-        preemptible = false
-      }
-    }
-    prod = {
-      masters = {
-        count       = 1
-        cpu         = 4
-        memory      = 4
-        disk        = 93
-        disk_type   = "network-ssd-nonreplicated"
-        preemptible = false
-      }
-      workers = {
-        count       = 6
-        cpu         = 2
-        memory      = 4
-        disk        = 20
-        disk_type   = "network-ssd"
-        preemptible = false
-      }
-    }
-  }
+#   k8s_cluster_resources = {
+#     stage = {
+#       masters = {
+#         count       = 1
+#         cpu         = 4
+#         memory      = 4
+#         disk        = 93
+#         disk_type   = "network-ssd-nonreplicated"
+#         preemptible = false
+#       }
+#       workers = {
+#         count       = 3
+#         cpu         = 2
+#         memory      = 4
+#         disk        = 20
+#         disk_type   = "network-ssd"
+#         preemptible = false
+#       }
+#     }
+#     prod = {
+#       masters = {
+#         count       = 1
+#         cpu         = 4
+#         memory      = 4
+#         disk        = 93
+#         disk_type   = "network-ssd-nonreplicated"
+#         preemptible = false
+#       }
+#       workers = {
+#         count       = 6
+#         cpu         = 2
+#         memory      = 4
+#         disk        = 20
+#         disk_type   = "network-ssd"
+#         preemptible = false
+#       }
+#     }
+#   }
 }
