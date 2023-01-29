43237023340430d876269ed088d980f851edcf1f
Sergey Chipyshev <schipyshev@stsoft.ru>
2023-01-29 17:14:57 +0500

    test sa2

--- terraform/k8s_sa.tf
+++ terraform/k8s_sa.tf
@@ -4,7 +4,7 @@ resource "yandex_iam_service_account" "k8s-robot" {
 }
 
 resource "yandex_resourcemanager_folder_iam_binding" "k8s-clusters-agent" {
-  folder_id = "${var.yandex_folder_id}"
+  folder_id = var.YANDEX_FOLDER_ID
   role      = "k8s.clusters.agent"
   members = [
     "serviceAccount:${yandex_iam_service_account.k8s-robot.id}"
@@ -12,14 +12,14 @@ resource "yandex_resourcemanager_folder_iam_binding" "k8s-clusters-agent" {
 }
 
 resource "yandex_resourcemanager_folder_iam_binding" "vpc-public-admin" {
-  folder_id = "${var.yandex_folder_id}"
+  folder_id = var.YANDEX_FOLDER_ID
   role      = "vpc.publicAdmin"
   members = [
     "serviceAccount:${yandex_iam_service_account.k8s-robot.id}"
   ]
 }
 resource "yandex_resourcemanager_folder_iam_binding" "images-puller" {
-  folder_id = "${var.yandex_folder_id}"
+  folder_id = var.YANDEX_FOLDER_ID
   role      = "container-registry.images.puller"
   members = [
     "serviceAccount:${yandex_iam_service_account.k8s-robot.id}"
@@ -28,7 +28,7 @@ resource "yandex_resourcemanager_folder_iam_binding" "images-puller" {
 
 #### Нужен ли он? Над подумать, а пока потестирую.
 resource "yandex_kms_symmetric_key_iam_binding" "viewer" {
-  symmetric_key_id = yandex_kms_symmetric_key.my-kms-key.id
+  symmetric_key_id = yandex_kms_symmetric_key.kms-key.id
   role             = "viewer"
   members = [
     "serviceAccount:${yandex_iam_service_account.k8s-robot.id}",
--- terraform/kms.tf
+++ terraform/kms.tf
@@ -1,7 +1,7 @@
 ## Позже удалю...
 resource "yandex_kms_symmetric_key" "kms-key" {
-  name              = "${var.TF_VAR_WORKSPACE_NAME}-symetric-key"
-  description       = "symetric key for workspace ${var.TF_VAR_WORKSPACE_NAME}"
+  name              = "symetric-key"
+  description       = "symetric key"
   default_algorithm = "AES_128"
   rotation_period   = "8760h"
 }
