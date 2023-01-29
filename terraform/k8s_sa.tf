b88d7f6f4e11759f05fba68b983bb79ea8e992a2
Sergey Chipyshev <schipyshev@stsoft.ru>
2023-01-29 17:08:48 +0500

    test sa1

--- terraform/k8s_sa.tf
+++ terraform/k8s_sa.tf
@@ -1,57 +1,36 @@
 resource "yandex_iam_service_account" "k8s-robot" {
-  name        = "${var.TF_VAR_WORKSPACE_NAME}-k8s-robot"
-  description = "${var.TF_VAR_WORKSPACE_NAME} K8S regional service account"
+  name        = "k8s-robot"
+  description = "K8S regional service account"
 }
 
 resource "yandex_resourcemanager_folder_iam_binding" "k8s-clusters-agent" {
-  folder_id = var.YANDEX_FOLDER_ID
+  folder_id = "${var.yandex_folder_id}"
   role      = "k8s.clusters.agent"
   members = [
-    "serviceAccount:${yandex_iam_service_account.k8s-robot.name}"
-  ]
-
-  depends_on = [
-    yandex_iam_service_account.k8s-robot
+    "serviceAccount:${yandex_iam_service_account.k8s-robot.id}"
   ]
 }
 
 resource "yandex_resourcemanager_folder_iam_binding" "vpc-public-admin" {
-  folder_id = var.YANDEX_FOLDER_ID
+  folder_id = "${var.yandex_folder_id}"
   role      = "vpc.publicAdmin"
   members = [
-    "serviceAccount:${yandex_iam_service_account.k8s-robot.name}"
-  ]
-
-  depends_on = [
-    yandex_iam_service_account.k8s-robot,
-    yandex_resourcemanager_folder_iam_binding.k8s-clusters-agent
+    "serviceAccount:${yandex_iam_service_account.k8s-robot.id}"
   ]
 }
-
 resource "yandex_resourcemanager_folder_iam_binding" "images-puller" {
-  folder_id = var.YANDEX_FOLDER_ID
+  folder_id = "${var.yandex_folder_id}"
   role      = "container-registry.images.puller"
   members = [
-    "serviceAccount:${yandex_iam_service_account.k8s-robot.name}"
-  ]
-
-  depends_on = [
-    yandex_iam_service_account.k8s-robot,
-    yandex_resourcemanager_folder_iam_binding.k8s-clusters-agent,
-    yandex_resourcemanager_folder_iam_binding.vpc-public-admin
+    "serviceAccount:${yandex_iam_service_account.k8s-robot.id}"
   ]
 }
-
 
 #### Нужен ли он? Над подумать, а пока потестирую.
 resource "yandex_kms_symmetric_key_iam_binding" "viewer" {
-  symmetric_key_id = yandex_kms_symmetric_key.kms-key.name
+  symmetric_key_id = yandex_kms_symmetric_key.my-kms-key.id
   role             = "viewer"
   members = [
-    "serviceAccount:${yandex_iam_service_account.k8s-robot.name}",
-  ]
-
-  depends_on = [
-    yandex_kms_symmetric_key.kms-key
+    "serviceAccount:${yandex_iam_service_account.k8s-robot.id}",
   ]
 }
