0cc9a0518bf87a270aa69d94edbb8b0e5f9ec9d3
Sergey Chipyshev <schipyshev@stsoft.ru>
2023-01-29 13:45:05 +0500

    edit sa1

---
+++ terraform/k8s_sa.tf
@@ -0,0 +1,36 @@
+resource "yandex_iam_service_account" "k8s-robot" {
+  name        = "k8s-robot"
+  description = "K8S regional service account"
+}
+
+resource "yandex_resourcemanager_folder_iam_binding" "k8s-clusters-agent" {
+  folder_id = var.YANDEX_FOLDER_ID
+  role      = "k8s.clusters.agent"
+  members = [
+    "serviceAccount:${yandex_iam_service_account.k8s-robot.id}"
+  ]
+}
+
+resource "yandex_resourcemanager_folder_iam_binding" "vpc-public-admin" {
+  folder_id = var.YANDEX_FOLDER_ID
+  role      = "vpc.publicAdmin"
+  members = [
+    "serviceAccount:${yandex_iam_service_account.k8s-robot.id}"
+  ]
+}
+
+resource "yandex_resourcemanager_folder_iam_binding" "images-puller" {
+  folder_id = var.YANDEX_FOLDER_ID
+  role      = "container-registry.images.puller"
+  members = [
+    "serviceAccount:${yandex_iam_service_account.k8s-robot.id}"
+  ]
+}
+
+resource "yandex_kms_symmetric_key_iam_binding" "viewer" {
+  symmetric_key_id = yandex_kms_symmetric_key.my-kms-key.id
+  role             = "viewer"
+  members = [
+    "serviceAccount:${yandex_iam_service_account.k8s-robot.id}",
+  ]
+}
