33621603cf140a0296309c2268df11c66bc344b1
Sergey Chipyshev <schipyshev@stsoft.ru>
2023-01-29 16:30:54 +0500

    edit sa11

--- terraform/k8s_sa.tf
+++ terraform/k8s_sa.tf
@@ -7,7 +7,7 @@ resource "yandex_resourcemanager_folder_iam_binding" "k8s-clusters-agent" {
   folder_id = var.YANDEX_FOLDER_ID
   role      = "k8s.clusters.agent"
   members = [
-    "serviceAccount:${yandex_iam_service_account.k8s-robot.name}"
+    "serviceAccount:${yandex_iam_service_account.k8s-robot.id.name}"
   ]
 }
 
@@ -15,7 +15,7 @@ resource "yandex_resourcemanager_folder_iam_binding" "vpc-public-admin" {
   folder_id = var.YANDEX_FOLDER_ID
   role      = "vpc.publicAdmin"
   members = [
-    "serviceAccount:${yandex_iam_service_account.k8s-robot.name}"
+    "serviceAccount:${yandex_iam_service_account.k8s-robot.id.name}"
   ]
 }
 
@@ -23,14 +23,14 @@ resource "yandex_resourcemanager_folder_iam_binding" "images-puller" {
   folder_id = var.YANDEX_FOLDER_ID
   role      = "container-registry.images.puller"
   members = [
-    "serviceAccount:${yandex_iam_service_account.k8s-robot.name}"
+    "serviceAccount:${yandex_iam_service_account.k8s-robot.id.name}"
   ]
 }
 
 
 #### Нужен ли он? Над подумать, а пока потестирую.
 resource "yandex_kms_symmetric_key_iam_binding" "viewer" {
-  symmetric_key_id = yandex_kms_symmetric_key.kms-key.name
+  symmetric_key_id = yandex_kms_symmetric_key.kms-key.id.name
   role             = "viewer"
   members = [
     "serviceAccount:${yandex_iam_service_account.k8s-robot.name}",
