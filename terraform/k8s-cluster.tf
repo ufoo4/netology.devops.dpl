20df7bff3228ec474d5183da4eca172d2a1460f8
Sergey Chipyshev <schipyshev@stsoft.ru>
2023-01-30 18:16:24 +0500

    test k8s 1

---
+++ terraform/k8s-cluster.tf
@@ -0,0 +1,34 @@
+resource "yandex_kubernetes_cluster" "k8s-regional-cluster" {
+  network_id = yandex_vpc_network.net.id
+  master {
+    version = var.K8S_VERSION
+    public_ip = true
+    regional {
+      region = "ru-central1"
+      location {
+        zone      = local.networks[count.index].zone_name
+        subnet_id = local.networks[count.index].subnet
+      }
+    #   location {
+    #     zone      = yandex_vpc_subnet.public-subnet-b.zone
+    #     subnet_id = yandex_vpc_subnet.public-subnet-b.id
+    #   }
+    #   location {
+    #     zone      = yandex_vpc_subnet.public-subnet-c.zone
+    #     subnet_id = yandex_vpc_subnet.public-subnet-c.id
+    #   }
+    }
+    security_group_ids = [yandex_vpc_security_group.k8s-main-sg.id]
+  }
+  service_account_id      = yandex_iam_service_account.k8s-robot.id
+  node_service_account_id = yandex_iam_service_account.k8s-robot.id
+  kms_provider {
+    key_id = yandex_kms_symmetric_key.kms-key.id
+  }
+
+  depends_on = [
+    yandex_resourcemanager_folder_iam_binding.k8s-clusters-agent,
+    yandex_resourcemanager_folder_iam_binding.vpc-public-admin,
+    yandex_resourcemanager_folder_iam_binding.images-puller
+  ]
+}
