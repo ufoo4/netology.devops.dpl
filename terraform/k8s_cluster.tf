c62b176b243216e62afeb8f57647d9477d4a6e26
Sergey Chipyshev <schipyshev@stsoft.ru>
2023-01-30 20:45:43 +0500

    test k8s 24

--- terraform/k8s_cluster.tf
+++ terraform/k8s_cluster.tf
@@ -1,9 +1,9 @@
 resource "yandex_kubernetes_cluster" "k8s_regional_cluster" {
-  name       = local.cluster_name
-  network_id = yandex_vpc_network.net.id
+  name            = local.cluster_name
+  network_id      = yandex_vpc_network.net.id
+  release_channel = "STABLE"
   master {
     version         = var.K8S_VERSION
-    release_channel = "STABLE"
     public_ip       = true
     regional {
       region = "ru-central1"
