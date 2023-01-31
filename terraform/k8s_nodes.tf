7dfaefddc5306bfa8462cf3a5e65aeccb959eb9b
Sergey Chipyshev <schipyshev@stsoft.ru>
2023-01-31 19:10:31 +0500

    test k8s 45

--- terraform/k8s_nodes.tf
+++ terraform/k8s_nodes.tf
@@ -1,7 +1,10 @@
 resource "yandex_kubernetes_node_group" "regional_node_group" {
-  cluster_id = yandex_kubernetes_cluster.k8s_regional_cluster.id
-  name       = "${var.TF_VAR_WORKSPACE_NAME}-regional-node-group"
-  version    = var.K8S_VERSION
+  cluster_id  = yandex_kubernetes_cluster.k8s_regional_cluster.id
+  name        = "${var.TF_VAR_WORKSPACE_NAME}-regional-node-group"
+  version     = var.K8S_VERSION
+  node_labels = {
+    "pool" = "${var.TF_VAR_WORKSPACE_NAME}"
+  }
 
   instance_template {
 
