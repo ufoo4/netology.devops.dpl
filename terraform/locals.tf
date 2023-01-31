6ba02a614437c33c023b2d90a0769e161627d986
Sergey Chipyshev <schipyshev@stsoft.ru>
2023-01-31 18:31:14 +0500

    test k8s 44

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -25,19 +25,19 @@ locals {
       workers = {
         scale         = 3
         cpu           = 2
-        core_fraction = 20
+        core_fraction = 5
         memory        = 1
         disk_size     = 64
         disk_type     = "network-hdd"
-        platform_id   = "standard-v3"
+        platform_id   = "standard-v1"
       }
     }
     prod = {
       workers = {
         scale         = 6
         cpu           = 2
-        core_fraction = 100
-        memory        = 4
+        core_fraction = 20
+        memory        = 2
         disk_size     = 64
         disk_type     = "network-ssd"
         platform_id   = "standard-v1"
