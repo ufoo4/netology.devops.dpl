ab9996e330b4d2c6fbe9808ab851f09bac0fc013
Sergey Chipyshev <schipyshev@stsoft.ru>
2023-02-03 11:21:38 +0500

    edit output

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,13 +1,13 @@
 output "external_v4_endpoint" {
   description = "An IPv4 external network address that is assigned to the master."
 
-  value = yandex_kubernetes_cluster.cluster.master[0].external_v4_endpoint
+  value = yandex_kubernetes_cluster.k8s_regional_cluster.master[0].external_v4_endpoint
 }
 
 output "internal_v4_endpoint" {
   description = "An IPv4 internal network address that is assigned to the master."
 
-  value = yandex_kubernetes_cluster.cluster.master[0].internal_v4_endpoint
+  value = yandex_kubernetes_cluster.k8s_regional_cluster.master[0].internal_v4_endpoint
 }
 
 output "cluster_ca_certificate" {
@@ -16,11 +16,11 @@ output "cluster_ca_certificate" {
   the Kubernetes cluster.
   EOF
 
-  value = yandex_kubernetes_cluster.cluster.master[0].cluster_ca_certificate
+  value = yandex_kubernetes_cluster.k8s_regional_cluster.master[0].cluster_ca_certificate
 }
 
 output "cluster_id" {
   description = "ID of a new Kubernetes cluster."
 
-  value = yandex_kubernetes_cluster.cluster.id
+  value = yandex_kubernetes_cluster.k8s_regional_cluster.id
 }
