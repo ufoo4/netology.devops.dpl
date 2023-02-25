9d0c126084b0624081b4290a44690a06dde544d6
Sergey Chipyshev <schipyshev@stsoft.ru>
2023-02-26 03:32:32 +0500

    minor fixes

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,10 +2,6 @@ output "external_v4_endpoint" {
   value = yandex_kubernetes_cluster.k8s_regional_cluster.master[0].external_v4_endpoint
 }
 
-# output "internal_v4_endpoint" {
-#   value = yandex_kubernetes_cluster.k8s_regional_cluster.master[0].internal_v4_endpoint
-# }
-
 output "cluster_ca_certificate" {
   value = yandex_kubernetes_cluster.k8s_regional_cluster.master[0].cluster_ca_certificate
 }
